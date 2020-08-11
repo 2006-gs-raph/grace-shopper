21b9ec9a9118bcbd0e2b47327c4ccea518560d89
Joncitone <jarreola7123@gmail.com>
2020-08-10 23:35:34 -0500

    created distinct cart get  routes and pseudeo-coded other routes, updated api orders and api index

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,71 +2,96 @@ const router = require('express').Router()
 const {Order, OrderProduct, Product} = require('../db/models')
 module.exports = router
 
-//needs to update to give back through-table information
-
+//finds or creates cart (Order with cart status) for logged-in and guest users
 router.get('/', async (req, res, next) => {
   try {
-    if (req.user) {
-      //Logged-In case
+    if (req.user.id) {
+      //Logged-in Case
       const userId = req.user.id
-      //use userId to find order with status cart
       const cart = await Order.findOrCreate({
         where: {
           userId,
           status: 'cart'
         }
       })
-      const orderId = cart[0].id
-      //use userId and orderId to find OrderProduct (through table)
-      //with the information needed to display cart details.
-      const cartDetails = await OrderProduct.findAll({
-        where: {
-          orderId
-        },
-        include: [{model: Product}]
-      })
-      res.json(cartDetails)
+      res.json(cart[0])
     } else {
       //Guest Case
-      if (!req.session.cart) {
-        req.session.cart = {}
-      }
-      res.json(req.session.cart)
+      const userId = req.session.id
+      const cart = await Order.findOrCreate({
+        where: {
+          userId,
+          status: 'cart'
+        }
+      })
+      res.json(cart[0])
     }
   } catch (err) {
     next(err)
   }
 })
 
-//Goal is to have the one order with status 'cart' update upon changes to redux
-//state, such as add and remove operations
-router.put('/', async (req, res, next) => {
+//gets information from OrderProduct through table based on orderId
+router.get('/:orderId', async (req, res, next) => {
   try {
-    const cart = await Order.findOne({
+    const {orderId} = req.params
+    //use orderId to findAll OrderProduct rows
+    //with the information needed to display cart details.
+    const cartDetails = await OrderProduct.findAll({
       where: {
-        userId: req.user.id,
-        status: 'cart'
+        orderId
       }
+      //include: [{model: Product}], (eager loading option)
     })
-    const userId = req.user.id
-    const orderId = cart.id
-    const [numUpdated, updatedOrderProduct] = await OrderProduct.update(
-      req.body,
-      {
-        where: {
-          userId,
-          orderId
-        },
-        returning: true,
-        plain: true
-      }
-    )
-    if (numUpdated) {
-      res.json(updatedOrderProduct[0])
-    } else {
-      res.status(400).end()
-    }
+    res.json(cartDetails)
   } catch (err) {
     next(err)
   }
 })
+
+/**
+ * ROUTES TO ADD
+ *
+ * (post route?)
+ * add product to cart route (find or create) if found update, update product qty based on difference between found qty and given qty
+ *      if not found create, decrement product qty based on given qty
+ *
+ * (delete route)
+ * remove product from cart route (destroy)
+ *
+ */
+
+/**
+ * OLD PUT ROUTE
+ */
+
+// router.put('/', async (req, res, next) => {
+//   try {
+//     const cart = await Order.findOne({
+//       where: {
+//         userId: req.user.id,
+//         status: 'cart',
+//       },
+//     })
+//     const userId = req.user.id
+//     const orderId = cart.id
+//     const [numUpdated, updatedOrderProduct] = await OrderProduct.update(
+//       req.body,
+//       {
+//         where: {
+//           userId,
+//           orderId,
+//         },
+//         returning: true,
+//         plain: true,
+//       }
+//     )
+//     if (numUpdated) {
+//       res.json(updatedOrderProduct[0])
+//     } else {
+//       res.status(400).end()
+//     }
+//   } catch (err) {
+//     next(err)
+//   }
+// })
