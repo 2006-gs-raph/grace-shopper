a61dcfbd5f43e940874a5c7fb920f8cca57cfb5f
Joncitone <jarreola7123@gmail.com>
2020-08-10 21:13:25 -0500

    added eager loading to cart route

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,16 +1,38 @@
 const router = require('express').Router()
-const {Order, OrderProduct} = require('../db/models')
+const {Order, OrderProduct, Product} = require('../db/models')
 module.exports = router
+
+//needs to update to give back through-table information
 
 router.get('/', async (req, res, next) => {
   try {
-    const cart = await Order.findOrCreate({
-      where: {
-        userId: req.user.id,
-        status: 'cart'
+    if (req.user) {
+      //Logged-In case
+      const userId = req.user.id
+      //use userId to find order with status cart
+      const cart = await Order.findOrCreate({
+        where: {
+          userId,
+          status: 'cart'
+        }
+      })
+      const orderId = cart[0].id
+      //use userId and orderId to find OrderProduct (through table)
+      //with the information needed to display cart details.
+      const cartDetails = await OrderProduct.findAll({
+        where: {
+          orderId
+        },
+        include: [{model: Product}]
+      })
+      res.json(cartDetails)
+    } else {
+      //Guest Case
+      if (!req.session.cart) {
+        req.session.cart = {}
       }
-    })
-    res.json(cart[0])
+      res.json(req.session.cart)
+    }
   } catch (err) {
     next(err)
   }
@@ -48,5 +70,3 @@ router.put('/', async (req, res, next) => {
     next(err)
   }
 })
-
-router.put('')
