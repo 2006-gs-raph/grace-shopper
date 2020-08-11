c3b17f1740330030937dced47eb43b4b45238335
Joncitone <jarreola7123@gmail.com>
2020-08-10 20:19:24 -0500

    initial work on cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,10 +2,21 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {fetchCart} from '../store/order'
+import {fetchCart} from '../store/cart'
+
+/**
+ *
+ * CART Component Needs:
+ * list of items and quantities in cart
+ * ability remove an item from cart
+ *
+ * checkout button (performs checkout operations)
+ *    updates order to 'completed' status and creates new empty cart
+ *    pushes confirmation page to history (navigates to confirmation page)
+ */
 
 const Cart = props => {
-  const {list, cart, selectedOrder, getCart} = props
+  const {list, getCart} = props
   useEffect(() => {
     getCart()
   }, [])
@@ -15,9 +26,7 @@ const Cart = props => {
 
 const mapState = state => {
   return {
-    list: state.order.list,
-    cart: state.order.cart,
-    selectedOrder: state.order.selectedOrder
+    list: state.order.list
   }
 }
 
